8b1692460153898a0636dceb0e2eafd2deff6b37
AndrewMundy <andrew.mundy@ieee.org>
2014-02-17 15:41:46 -0500

    Structure for dimension source.

---
+++ DimensionSource/dimension_source.c
@@ -0,0 +1,9 @@
+#include "dimension_source.h"
+
+int main( void ) {
+  // Set up routing tables, for this test this should be comparatively simple
+}
+
+void timer_callback( uint arg0, uint arg1 ) {
+  // Set some predefined values per dimension
+}
---
+++ DimensionSource/dimension_source.h
@@ -0,0 +1,9 @@
+#include "sark.h"
+#include "spin1_api.h"
+
+/*
+ * Necessarily hacky test source.
+ */
+
+int main( void );
+void timer_callback( uint unused0, uint unused1 );
