9cfbf7fcf91b54f26d32c8ef50ec676cb92a484c
AndrewMundy <andrew.mundy@ieee.org>
2014-02-17 17:25:37 -0500

    Initial stab at working dimension source.

--- dimension_source.c
+++ dimension_source.c
@@ -1,9 +1,12 @@
 #include "dimension_source.h"
 
-int main( void ) {
+int c_main( void ) {
   // Set up routing tables, for this test this should be comparatively simple
+  spin1_callback_on( TIMER_TICK, timer_callback, 0 );
 }
 
-void timer_callback( uint arg0, uint arg1 ) {
+void timer_callback( uint simulation_time, uint none ) {
   // Set some predefined values per dimension
+  accum val = 0.5;
+  spin1_send_mc_packet( 0x0000, (uint) val, 1 );
 }
--- dimension_source.h
+++ dimension_source.h
@@ -1,9 +1,9 @@
-#include "sark.h"
 #include "spin1_api.h"
+#include "../common/stdfix-full-iso.h"
 
 /*
  * Necessarily hacky test source.
  */
 
 int main( void );
-void timer_callback( uint unused0, uint unused1 );
+void timer_callback( uint simulation_time, uint none );
