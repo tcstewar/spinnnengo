72a4f792dbee90ba851488212e8f2e2a6d173343
AndrewMundy <andrew.mundy@ieee.org>
2014-02-17 20:36:20 -0500

    Moved curly brackets to match standard from other code.

--- dimension_source/dimension_source.c
+++ dimension_source/dimension_source.c
@@ -1,6 +1,7 @@
 #include "dimension_source.h"
 
-int c_main( void ) {
+int c_main( void )
+{
   // Enable the timer tick callback
   spin1_set_timer_tick( 1000 ); // Timer tick / ms
   spin1_callback_on( TIMER_TICK, timer_callback, 0 );
@@ -12,7 +13,8 @@ int c_main( void ) {
   spin1_start( );
 }
 
-void timer_callback( uint simulation_time, uint none ) {
+void timer_callback( uint simulation_time, uint none )
+{
   // Set some predefined values per dimension
   accum val = 0.5;
   spin1_send_mc_packet( 0x0000, (uint) val, 1 );
